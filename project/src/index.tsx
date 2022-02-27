edbd6c4341433003de2ab8b025f75c6cf61eed27
Kravtsov Ivan <rhtr@mail.ru>
2022-02-27 19:44:37 +0300

    Начинаем программировать

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -1,9 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
+import { GENRES } from './utils/const';
+
+
+const settings = {
+  genres: Object.values(GENRES),
+  cardsCount: 20,
+  promoFilmCard: {
+    title: 'The Grand Budapest Hotel',
+    genre: GENRES.DRAMS,
+    year: 2014,
+  },
+  filmCard: {
+    image: {
+      imageSrc:  'img/fantastic-beasts-the-crimes-of-grindelwald.jpg ',
+      imageAlt:  'Fantastic Beasts: The Crimes of Grindelwald ',
+    },
+    filmHref:  'film-page.html ',
+    filmTitle:  'Fantastic Beasts: The Crimes of Grindelwald ',
+  },
+};
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App promoFilmCard={settings.promoFilmCard} filmCard={settings.filmCard} cardsCount={settings.cardsCount} genres={settings.genres} />
   </React.StrictMode>,
   document.getElementById('root'));
---
+++ project/src/pages/film/film.tsx
@@ -0,0 +1,104 @@
+import Header from '../../components/header/header';
+import Footer from '../../components/footer/footer';
+import Overview from '../../components/overview/overview';
+import FilmCard from '../../components/film-card/film-card';
+
+type FilmScreenProps = {
+  cardsCount: number,
+  promoFilmCard: {
+    title: string,
+    genre: string,
+    year: number,
+  },
+  filmCard: {
+    image:  {
+      imageSrc:  string,
+      imageAlt:  string,
+    },
+    filmHref:  string,
+    filmTitle:  string,
+  }
+};
+
+function Film({promoFilmCard, filmCard, cardsCount}: FilmScreenProps): JSX.Element {
+  return (
+    <>
+      <section className="film-card film-card--full">
+        <div className="film-card__hero">
+          <div className="film-card__bg">
+            <img src="img/bg-the-grand-budapest-hotel.jpg" alt="The Grand Budapest Hotel" />
+          </div>
+
+          <h1 className="visually-hidden">WTW</h1>
+
+          <Header />
+
+          <div className="film-card__wrap">
+            <div className="film-card__desc">
+              <h2 className="film-card__title">{promoFilmCard.title}</h2>
+              <p className="film-card__meta">
+                <span className="film-card__genre">{promoFilmCard.genre}</span>
+                <span className="film-card__year">{promoFilmCard.year}</span>
+              </p>
+
+              <div className="film-card__buttons">
+                <button className="btn btn--play film-card__button" type="button">
+                  <svg viewBox="0 0 19 19" width="19" height="19">
+                    <use xlinkHref="#play-s"></use>
+                  </svg>
+                  <span>Play</span>
+                </button>
+                <button className="btn btn--list film-card__button" type="button">
+                  <svg viewBox="0 0 19 20" width="19" height="20">
+                    <use xlinkHref="#add"></use>
+                  </svg>
+                  <span>My list</span>
+                </button>
+                <a href="add-review.html" className="btn film-card__button">Add review</a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="film-card__wrap film-card__translate-top">
+          <div className="film-card__info">
+            <div className="film-card__poster film-card__poster--big">
+              <img src="img/the-grand-budapest-hotel-poster.jpg" alt="The Grand Budapest Hotel poster" width="218" height="327" />
+            </div>
+
+            <div className="film-card__desc">
+              <nav className="film-nav film-card__nav">
+                <ul className="film-nav__list">
+                  <li className="film-nav__item film-nav__item--active">
+                    <a href="#" className="film-nav__link">Overview</a>
+                  </li>
+                  <li className="film-nav__item">
+                    <a href="#" className="film-nav__link">Details</a>
+                  </li>
+                  <li className="film-nav__item">
+                    <a href="#" className="film-nav__link">Reviews</a>
+                  </li>
+                </ul>
+              </nav>
+              <Overview />
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <div className="page-content">
+        <section className="catalog catalog--like-this">
+          <h2 className="catalog__title">More like this</h2>
+
+          <div className="catalog__films-list">
+            {new Array(cardsCount).fill(<FilmCard filmCard={filmCard} />)}
+          </div>
+        </section>
+
+        <Footer />
+      </div>
+    </>
+  );
+}
+
+export default Film;
